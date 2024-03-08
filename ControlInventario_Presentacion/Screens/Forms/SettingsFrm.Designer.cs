e6d21bec486db888e009a87f3a72e114d373ee57
JuanDiego200209 <juan_Diego_39@hotmail.com>
2024-03-08 16:59:02 -0400

    diseño settings

--- ControlInventario_Presentacion/Screens/Forms/SettingsFrm.Designer.cs
+++ ControlInventario_Presentacion/Screens/Forms/SettingsFrm.Designer.cs
@@ -39,81 +39,87 @@
             checkBox2 = new CheckBox();
             checkBox3 = new CheckBox();
             dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // lblTitleSettings
             // 
             lblTitleSettings.AutoSize = true;
-            lblTitleSettings.Font = new Font("Segoe UI Variable Display", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTitleSettings.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
             lblTitleSettings.ForeColor = Color.Gainsboro;
-            lblTitleSettings.Location = new Point(90, 34);
+            lblTitleSettings.Location = new Point(79, 26);
             lblTitleSettings.Name = "lblTitleSettings";
-            lblTitleSettings.Size = new Size(114, 31);
+            lblTitleSettings.Size = new Size(103, 24);
             lblTitleSettings.TabIndex = 2;
             lblTitleSettings.Text = "SETTINGS";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.BackColor = Color.Black;
+            label1.BackColor = Color.Transparent;
             label1.ForeColor = Color.White;
-            label1.Location = new Point(90, 98);
+            label1.Location = new Point(40, 43);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(38, 15);
             label1.TabIndex = 3;
             label1.Text = "label1";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(182, 95);
+            textBox1.Location = new Point(120, 40);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(267, 27);
+            textBox1.Size = new Size(234, 23);
             textBox1.TabIndex = 4;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.BackColor = Color.Black;
+            label2.BackColor = Color.Transparent;
             label2.ForeColor = Color.White;
-            label2.Location = new Point(90, 176);
+            label2.Location = new Point(40, 101);
             label2.Name = "label2";
-            label2.Size = new Size(50, 20);
+            label2.Size = new Size(38, 15);
             label2.TabIndex = 3;
             label2.Text = "label2";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(182, 173);
+            textBox2.Location = new Point(120, 99);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(267, 27);
+            textBox2.Size = new Size(234, 23);
             textBox2.TabIndex = 4;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.BackColor = Color.Black;
+            label3.BackColor = Color.Transparent;
             label3.ForeColor = Color.White;
-            label3.Location = new Point(90, 262);
+            label3.Location = new Point(40, 165);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(38, 15);
             label3.TabIndex = 3;
             label3.Text = "label3";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(182, 259);
+            textBox3.Location = new Point(120, 163);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(267, 27);
+            textBox3.Size = new Size(234, 23);
             textBox3.TabIndex = 4;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
             checkBox1.ForeColor = Color.White;
-            checkBox1.Location = new Point(755, 360);
+            checkBox1.Location = new Point(661, 270);
+            checkBox1.Margin = new Padding(3, 2, 3, 2);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(101, 24);
+            checkBox1.Size = new Size(83, 19);
             checkBox1.TabIndex = 5;
             checkBox1.Text = "checkBox1";
             checkBox1.UseVisualStyleBackColor = true;
@@ -122,9 +128,10 @@
             // 
             checkBox2.AutoSize = true;
             checkBox2.ForeColor = Color.White;
-            checkBox2.Location = new Point(755, 406);
+            checkBox2.Location = new Point(661, 304);
+            checkBox2.Margin = new Padding(3, 2, 3, 2);
             checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(101, 24);
+            checkBox2.Size = new Size(83, 19);
             checkBox2.TabIndex = 5;
             checkBox2.Text = "checkBox2";
             checkBox2.UseVisualStyleBackColor = true;
@@ -133,9 +140,10 @@
             // 
             checkBox3.AutoSize = true;
             checkBox3.ForeColor = Color.White;
-            checkBox3.Location = new Point(755, 455);
+            checkBox3.Location = new Point(661, 341);
+            checkBox3.Margin = new Padding(3, 2, 3, 2);
             checkBox3.Name = "checkBox3";
-            checkBox3.Size = new Size(101, 24);
+            checkBox3.Size = new Size(83, 19);
             checkBox3.TabIndex = 5;
             checkBox3.Text = "checkBox3";
             checkBox3.UseVisualStyleBackColor = true;
@@ -143,33 +151,48 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(496, 95);
+            dataGridView1.Location = new Point(468, 87);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(781, 191);
+            dataGridView1.Size = new Size(485, 143);
             dataGridView1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.DimGray;
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Location = new Point(41, 76);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(408, 204);
+            groupBox1.TabIndex = 7;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
             // 
             // SettingsFrm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
-            ClientSize = new Size(1329, 653);
+            ClientSize = new Size(982, 490);
+            Controls.Add(groupBox1);
             Controls.Add(dataGridView1);
             Controls.Add(checkBox3);
             Controls.Add(checkBox2);
             Controls.Add(checkBox1);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(lblTitleSettings);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "SettingsFrm";
             Text = "SettingsFrm";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -187,5 +210,6 @@
         private CheckBox checkBox2;
         private CheckBox checkBox3;
         private DataGridView dataGridView1;
+        private GroupBox groupBox1;
     }
 }
