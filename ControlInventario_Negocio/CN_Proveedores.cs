2ebcb6d86d2b0a2262aa2e5a1c1615b2d1370fd5
Samuel Ramirez <samuelramirezsuriel@outlook.com>
2024-03-18 19:01:35 -0400

    updates

--- ControlInventario_Negocio/CN_Proveedores.cs
+++ ControlInventario_Negocio/CN_Proveedores.cs
@@ -1,11 +1,6 @@
 ﻿using ControlInventario_Datos;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ControlInventario_Negocio
 {
@@ -81,7 +76,7 @@ namespace ControlInventario_Negocio
             comando.Parameters.Clear();
             conexion.CerrarConexion();
         }
-              
+
 
     }
 }
--- ControlInventario_Presentacion/Screens/Forms/VentasFrm.cs
+++ ControlInventario_Presentacion/Screens/Forms/VentasFrm.cs
@@ -1,16 +1,8 @@
 ﻿using ControlInventario_Datos;
 using ControlInventario_Negocio;
 using ControlInventario_Presentacion.Clases;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace ControlInventario_Presentacion.Screens.Forms
 {
